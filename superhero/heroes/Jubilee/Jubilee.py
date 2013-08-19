c962aef7d876677600719f37c6a864496856e9b4
Neo-San <ericj.piton@yahoo.com>
2013-08-19 16:45:10 -0500

    Update Jubilee.py
    Jubilee new code Tested and working on the new system. Should still test on linux to be sure.

--- superhero/heroes/Jubilee/Jubilee.py
+++ superhero/heroes/Jubilee/Jubilee.py
@@ -3,6 +3,8 @@ import playerlib
 import gamethread
 import time
 superhero = es.import_addon('superhero')
+global gusers
+gusers = {}
 
 def load():
     es.dbgmsg(0, "[SH] Successfully loaded Jubilee")
@@ -11,30 +13,31 @@ def unload():
     gamethread.cancelDelayed("Unblind")
     
 def player_spawn(ev):
+    global gusers
     userid = ev['userid']
-    if es.exists('userid',userid):
-        if superhero.hasHero(userid,'Jubilee'):
-            player = playerlib.getPlayer(userid)
-            if not playerlib.getPlayer(userid).isdead:
-                superhero.Users[userid]['jub_cooldown'] = int(time.time()) + 5
-                superhero.Users[userid]['jub_protect'] = 0
+    if superhero.hasHero(userid,'Jubilee'):
+        player = playerlib.getPlayer(userid)
+        if not playerlib.getPlayer(userid).isdead:
+            gusers[userid] = {}
+            gusers[ev['userid']]['jub_cooldown'] = int(time.time())
+            gusers[userid]['jub_protect'] = 0
             
 def power():
     userid = str(es.getcmduserid())
+    if not es.exists('userid',userid):
+        return
     player = playerlib.getPlayer(userid)
     if int(player.isdead) != 1:
-        powerx = str(superhero.Users[userid]['powerx'])
-        if superhero.Users[userid][powerx] == 'Jubilee':
-            if not 'jub_cooldown' in superhero.Users[userid]:
-                superhero.Users[userid]['jub_cooldown'] = int(time.time()) + 5
-            if int(time.time()) >= int(superhero.Users[userid]['jub_cooldown']):
-                es.tell(userid, '#multi', '#green[SH]#lightgreen Jubilee pink shades now protect you from flashbangs')
-                fade(userid, 0, 5, 5, 64, 0, 64, 130)
-                superhero.Users[userid]['jub_protect'] = 1
-                superhero.Users[userid]['jub_cooldown'] = int(time.time()) + 30
-                gamethread.delayed(10, Unblind, userid)
-            else:
-                es.tell(userid, '#multi', '#green[SH]#lightgreen Cannot activate Jubilee #green',int(superhero.Users[userid]['jub_cooldown'])-int(time.time()),'#lightgreenseconds left')
+        if not 'jub_cooldown' in gusers[userid]:
+            gusers[userid]['jub_cooldown'] = int(time.time()) + 5
+        if int(time.time()) >= int(gusers[userid]['jub_cooldown']):
+            es.tell(userid, '#multi', '#green[SH]#lightgreen Jubilee pink shades now protect you from flashbangs')
+            fade(userid, 0, 5, 5, 64, 0, 64, 130)
+            gusers[userid]['jub_protect'] = 1
+            gusers[userid]['jub_cooldown'] = int(time.time()) + 30
+            gamethread.delayed(10, Unblind, userid)
+        else:
+            es.tell(userid, '#multi', '#green[SH]#lightgreen Cannot activate Jubilee #green',int(gusers[userid]['jub_cooldown'])-int(time.time()),'#lightgreenseconds left')
             
 def fade(users, type, fadetime, totaltime, r, g, b, a):
     t = int(type)
@@ -58,15 +61,16 @@ def fade(users, type, fadetime, totaltime, r, g, b, a):
 def player_blind(ev):
     userid = ev['userid']
     if superhero.hasHero(userid,'Jubilee'):
-        if not 'jub_protect' in superhero.Users[userid]:
-            superhero.Users[userid]['jub_protect'] = 0
-        elif superhero.Users[userid]['jub_protect'] == 1:
-            playerBlinded(userid)
+        global gusers
+        gusers[userid] = {}
+        if not 'jub_protect' in gusers[userid]:
+            gusers[userid]['jub_protect'] = 0
+        elif gusers[userid]['jub_protect'] == 1:
+	    es.setplayerprop(userid, 'CCSPlayer.m_flFlashMaxAlpha', 0)
+	    es.setplayerprop(userid, 'CCSPlayer.m_flFlashDuration', 0)
 
-def playerBlinded(userid):
-		es.setplayerprop(userid, 'CCSPlayer.m_flFlashMaxAlpha', 0)
-		es.setplayerprop(userid, 'CCSPlayer.m_flFlashDuration', 0)
         
 def Unblind(userid):
-    superhero.Users[userid]['jub_protect'] = 0
+    global gusers
+    gusers[userid]['jub_protect'] = 0
     es.tell(userid, '#multi', '#green[SH]#lightgreen Jubilee protection has ended, now you are vulnerable to flashes')

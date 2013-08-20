510b4ef325830b19e8b273ff6d7e6cb3f0cd6b42
Neo-San <ericj.piton@yahoo.com>
2013-08-20 17:17:29 -0500

    Update Invisible Man.py
    fix invisible man. the hero will work one round restart. if you pick the hero in game you must wait until the round restarts or rejoin. otherwise we get errors trying to make it work both ways

--- superhero/heroes/Invisible Man/Invisible Man.py
+++ superhero/heroes/Invisible Man/Invisible Man.py
@@ -1,4 +1,3 @@
-##this code works but the delays need to be tweaked a bit
 import es
 import playerlib
 import effectlib
@@ -20,8 +19,17 @@ def es_map_start(ev):
     gamethread.cancelDelayed(check_moving)
 
 def player_spawn(ev):
-    userid = ev['userid']
     global gusers
+    userid = ev['userid']
+    gusers[userid] = {}
+    if superhero.hasHero(userid,'Invisible Man'):
+        gamethread.cancelDelayed(check_moving)
+        es.delayed(1,'es_xdoblock superhero/heroes/Invisible Man/check_moving')
+
+def selected():
+    global gusers
+    userid = es.getcmduserid()
+    gusers[userid] = {}
     if superhero.hasHero(userid,'Invisible Man'):
         es.delayed(1,'es_xdoblock superhero/heroes/Invisible Man/check_moving')
 
@@ -29,10 +37,9 @@ def round_end(ev):
     gamethread.cancelDelayed(check_moving)
  
 def player_footstep(ev):
+    global gusers
     userid = ev['userid']
     if superhero.hasHero(userid,'Invisible Man'):
-        global gusers
-        gusers[userid] = {}
         player = playerlib.getPlayer(userid)
         r,g,b,a = player.getColor() 
         player.set("color", [r, g, b, 255])
@@ -43,8 +50,6 @@ def player_footstep(ev):
 def player_hurt(ev):
     userid = ev['userid']
     if superhero.hasHero(userid,'Invisible Man'):
-        global gusers
-        gusers[userid] = {}
         player = playerlib.getPlayer(userid)
         r,g,b,a = player.getColor() 
         player.set("color", [r, g, b, 255])
@@ -55,8 +60,6 @@ def player_hurt(ev):
 def weapon_fire(ev):
     userid = ev['userid']
     if superhero.hasHero(userid,'Invisible Man'):
-        global gusers
-        gusers[userid] = {}
         player = playerlib.getPlayer(userid)
         r,g,b,a = player.getColor() 
         player.set("color", [r, g, b, 255])
@@ -65,27 +68,22 @@ def weapon_fire(ev):
             es.centertell(userid,"You are not cloaked anymore!")
     
 def check_moving():
-    for userid in es.getUseridList():
-        global gusers
-        gusers[userid] = {}
-        playerList = playerlib.getPlayerList('#alive')
-        for ply in playerList:
-            if not es.exists('userid',userid):
-                return
-            if superhero.hasHero(userid,'Invisible Man'):   
-                player = playerlib.getPlayer(userid)
-                if not playerlib.getPlayer(userid).isdead:
-                    if not 'oldpos' in gusers[userid]: gusers[userid]['oldpos'] = 0
-                    newpos = es.getplayerlocation(userid)
-                    oldpos = gusers[userid]['oldpos']
-                    if str(newpos) == str(oldpos):
-                        r,g,b,a = player.getColor() 
-                        if a >= 15:
-                            a -= 15
-                        invis = a
-                        invis = invis * 100 / 255
-                        invis = 100-invis
-                        es.centertell(userid,invis,'% Cloaked')
-                        player.set("color", [r, g, b, a])                       
-                    gusers[userid]['oldpos'] = newpos   
-        es.delayed(6,'es_xdoblock superhero/heroes/Invisible Man/check_moving')
+    global gusers
+    for userid in gusers:
+        if superhero.hasHero(userid,'Invisible Man'):   
+            player = playerlib.getPlayer(userid)
+            if not playerlib.getPlayer(userid).isdead:
+                if not 'oldpos' in gusers[userid]: gusers[userid]['oldpos'] = 0
+                newpos = es.getplayerlocation(userid)
+                oldpos = gusers[userid]['oldpos']
+                if str(newpos) == str(oldpos):
+                    r,g,b,a = player.getColor() 
+                    if a >= 15:
+                        a -= 15
+                    invis = a
+                    invis = invis * 100 / 255
+                    invis = 100-invis
+                    es.centertell(userid,invis,'% Cloaked')
+                    player.set("color", [r, g, b, a])                       
+                gusers[userid]['oldpos'] = newpos   
+    es.delayed(0.4,'es_xdoblock superhero/heroes/Invisible Man/check_moving')
